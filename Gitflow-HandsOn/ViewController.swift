fe8d998b8ce6d13893382a5ed8ece3e6e7b89c5e
muhammadtafanirabbani <ratafani@gmail.com>
2020-05-20 11:17:13 +0700

    Button Magic Pressed Finished

--- Gitflow-HandsOn/ViewController.swift
+++ Gitflow-HandsOn/ViewController.swift
@@ -22,7 +22,18 @@ class ViewController: UIViewController {
     // Will change the UIView background acording tothe button color.
     @IBAction func colorButtonPressed(_ sender: UIButton) {
         // do your code here
+        //func random Color
+        let red : CGFloat = .random(in: 0...0.8)
+        let blue : CGFloat = .random(in: 0...1)
+        let green : CGFloat = .random(in: 0.4...1)
+        let color = UIColor.init(red: red, green: green, blue: blue, alpha: 1)
+        UIView.animate(withDuration: 0.7) {
+            self.view.backgroundColor = self.colorButton.backgroundColor
+            self.colorButton.backgroundColor = color
+        }
+        
     }
+    
     
     
     @IBAction func resetButtonPressed(_ sender: UIButton) {
